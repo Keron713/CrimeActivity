ba021ea2ac5fb6dd8dc140f249391710a4b49878
Keron713 <keron713@gmail.com>
2021-01-21 21:33:28 +0200

    Initial commit

---
+++ app/src/main/java/com/homework/crimeactivity/CrimeLab.java
@@ -0,0 +1,50 @@
+package com.homework.crimeactivity;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private Map<UUID,Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+//        mCrimes = new ArrayList<>();
+        mCrimes = new LinkedHashMap<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); // Для каждого второго объекта
+//            mCrimes.add(crime);
+            mCrimes.put(crime.getId(), crime);
+        }
+    }
+
+    public List<Crime> getCrimes() {
+//        return mCrimes;
+        return new ArrayList<>(mCrimes.values());
+    }
+
+    public Crime getCrime(UUID id) {
+//        for (Crime crime : mCrimes) {
+//            if (crime.getId().equals(id)) {
+//                return crime;
+//            }
+//        }
+//        return null;
+        return mCrimes.get(id);
+    }
+}
---
+++ app/src/main/java/com/homework/crimeactivity/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.homework.crimeactivity;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
